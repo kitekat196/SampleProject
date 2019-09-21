556c1375d8b319a6f940c3dd5f3f18810e46c727
Данила Морозов <89854821428@yandex.ru>
2019-09-21 13:31:25 +0300

    all types Java

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,24 @@
+package lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Alloha world");
+        byte number1 = 0;   /* 1 byte */
+        int number2 = 0;    /* 4byte */
+        float number3 = 0.1f;   /* 4byte */
+        short number4 = 0;  /* 2byte */
+        long number5 = 0L;  /* 8byte */
+        double number6 = 0.1d;  /* 8byte */
+        char word1 = 'N';   /* 16bit */
+        boolean number7 = true; /* true/false */
+
+
+
+
+
+
+
+    }
+
+}
