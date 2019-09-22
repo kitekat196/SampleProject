3981ff71b0d62e33b73ecea261676a7b2151964f
Данила Морозов <89854821428@yandex.ru>
2019-09-22 10:30:39 +0300

    lesson2

--- src/main/java/lesson1/justice.java
+++ src/main/java/lesson1/justice.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 public class justice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count = -1;
-        int sum = 0;
-        int nomMoney = 0;
-        while (count != 0)
-        {
-            System.out.println("Enter money");
-            count = sc.nextInt();
-            if(count == 1 || count == 2 || count == 5 || count == 10){
-                sum = sum + count;
-                if(nomMoney < count)
-                {
-                    nomMoney = count;
-                }
-            }
-            else
-            {
-                System.out.println("Error");
-            }
+        int num1, num2, num3;
+        System.out.println("num1");
+        num1 = sc.nextInt();
+        System.out.println("num2");
+        num2 = sc.nextInt();
+        System.out.println("num3");
+        num3 = sc.nextInt();
+
+        if(num1 > num2 && num1 > num3) {
+            System.out.println(num1);
         }
-        System.out.println("sum muney = "+sum +"\n"+ "max nom. money = " + nomMoney);
+        else if(num2 > num1 && num2 > num3){
+            System.out.println(num2);
+        }
+        else if (num3 > num1 && num3 > num2) {
+            System.out.println(num3);
+        }
+        else if(num1 == num2 && num1 == num3){
+            System.out.println(num1+" = "+ num2+ " = "+num3);
+        }
+        else
+        {
+            System.out.println("Error");
+        }
     }
 }
