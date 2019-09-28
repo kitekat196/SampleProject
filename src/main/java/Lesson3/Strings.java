288bf38bbb6227f2799298f0cd7ebf319bb3d8e1
Данила Морозов <89854821428@yandex.ru>
2019-09-28 13:45:33 +0300

    TurnOverString

--- src/main/java/Lesson3/Strings.java
+++ src/main/java/Lesson3/Strings.java
@@ -1,5 +1,15 @@
 package Lesson3;
 
-public class Strings
-{
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String str2 = "";
+        for (int i = str.length()-1; i > -1; i--) {
+            str2 = str2 + str.charAt(i);
+        }
+        System.out.print(str2);
+    }
 }
