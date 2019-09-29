1244cff7b5f39e33373b05b0a7a50548f0c349cb
Катя <gitler.kaka@mai.ru>
2019-09-29 13:47:05 +0300

    Homework

--- src/main/java/Homework/Lesson3.java
+++ src/main/java/Homework/Lesson3.java
@@ -1,5 +1,32 @@
 package Homework;
 
+import java.util.Scanner;
+
 public class Lesson3
 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] badwords = {"мыло", "мать", "катя"};
+        String str = sc.nextLine();
+        boolean check = false;
+        if(str.startsWith("здравствуйте") == true)
+        {
+
+            for (int i = 0; i < badwords.length; i++)
+            {
+                if (str.contains(badwords[i]) == true)
+                {
+                    check = true;
+                    break;
+                }
+            }
+        } else {
+            check = true;
+        }
+        if(check == true) {
+            System.out.println("govno text");
+        } else {
+            System.out.println("The text was successfully verified");
+        }
+    }
 }
