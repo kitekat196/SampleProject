17fa49e14004c9c742b53a303b26a750860fe81b
Катя <gitler.kaka@mai.ru>
2019-10-02 21:10:54 +0300

    homework

--- src/main/java/Homework/Lesson4.java
+++ src/main/java/Homework/Lesson4.java
@@ -1,5 +1,33 @@
 package Homework;
+public class Lesson4 {
 
-public class Lesson4
-{
+    static String play(String[] roles, String[] textLines) {
+        String str = "";
+        for (int i = 0; i < roles.length; i++) {
+            str = str + roles[i] + ": \n";
+            for (int j = 0; j < textLines.length; j++) {
+                if(textLines[j].startsWith(roles[i] + ": ")){
+                    str = str + (j+1)+") ";
+                    for (int k = roles[i].length()+2; k < textLines[j].length(); k++) {
+                        str = str + textLines[j].charAt(k);
+                    }
+                    str = str + "\n";
+                }
+            }
+            str = str + "\n";
+        }
+        return str;
+    }
+    public static void main(String[] args) {
+        String[] roles = {"Городничий", "Аммос Федорович", "Артемий Филиппович", "Лука Лукич"};
+        String[] textLines = {
+                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!" };
+        System.out.println(play(roles, textLines));
+    }
 }
