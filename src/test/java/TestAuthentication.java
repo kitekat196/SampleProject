77dae50408f8c1bb136daa32e4d26acf2dfd0770
Катя <gitler.kaka@mai.ru>
2019-10-08 22:03:23 +0300

    TestClass

--- src/test/java/TestAuthentication.java
+++ src/test/java/TestAuthentication.java
@@ -1,10 +1,7 @@
-import Homework.Lesson6_FuncMap_Unique;
-import Homework.Lesson6_symmetricDifference;
+
 import Lesson4.Authentication;
 import Lesson4.ArrayExaple;
 import org.junit.jupiter.api.Test;
-
-import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestAuthentication
@@ -51,40 +48,6 @@ public class TestAuthentication
         int[] mas = {};
         float res = ArrayExaple.SrATest(mas);
         assertEquals(0,res);
-    }
-    @Test
-    public  void  countInListTest() {
-        List<String> list = new ArrayList<>();
-        list.add("cola");  list.add("pepsi");
-        list.add("cola");  list.add("pepsi");
-        list.add("cola");  list.add("pepsi");
-        list.add("cola");  list.add("fanta");
-        list.add("fanta"); list.add("sprite");
-        Map<String,Integer> map = new HashMap<>();
-        map.put("cola", 4);
-        map.put("pepsi", 3);
-        map.put("fanta", 2);
-        map.put("sprite",1);
-        Map<String,Integer> res = Lesson6_FuncMap_Unique.countInList(list);
-        assertEquals(map,res);
-    }
-    @Test void SymmetricDifferenceTest() {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        set1.add(1);        set2.add(4);
-        set1.add(2);        set2.add(5);
-        set1.add(3);        set2.add(6);
-        set1.add(4);        set2.add(7);
-        set1.add(5);        set2.add(8);
-        Set<Integer> setInt = Lesson6_symmetricDifference.symmetricDifference(set1,set2);
-        Set<Integer> res = new HashSet<>();
-        res.add(1);
-        res.add(2);
-        res.add(3);
-        res.add(6);
-        res.add(7);
-        res.add(8);
-        assertEquals(res,setInt);
     }
 
 }
