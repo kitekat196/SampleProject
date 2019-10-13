683847498ca3cf95d53e8d7d83580e8589afd3b5
Катя <gitler.kaka@mai.ru>
2019-10-13 23:01:46 +0300

    LessonException

--- src/main/java/Lesson8/LessonException/LessonException.java
+++ src/main/java/Lesson8/LessonException/LessonException.java
@@ -1,4 +1,21 @@
 package Lesson8.LessonException;
+import java.util.Scanner;
 
 public class LessonException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two number\nNumber One");
+        int a = sc.nextInt();
+        System.out.println("Number two");
+        int b = sc.nextInt();
+        try {
+            System.out.println(a/b);
+        }catch (Exception x){
+            System.out.println(x.getMessage());
+
+        }
+
+    }
 }
+
+//e.printStackTrace
