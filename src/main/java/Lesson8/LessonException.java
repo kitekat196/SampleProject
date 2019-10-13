a57444778ed4df1cbf88e931aa965c1a5c472e14
Данила Морозов <89854821428@yandex.ru>
2019-10-13 11:00:13 +0300

    Exception

---
+++ src/main/java/Lesson8/LessonException.java
@@ -0,0 +1,21 @@
+package Lesson8;
+import java.util.Scanner;
+
+public class LessonException {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two number\nNumber One");
+		int a = sc.nextInt();
+		System.out.println("Number two");
+		int b = sc.nextInt();
+		try {
+			System.out.println(a/b);
+		}catch (Exception x){
+			System.out.println(x.getMessage());
+
+		}
+
+	}
+}
+
+//e.printStackTrace
