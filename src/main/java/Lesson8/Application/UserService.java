ea89a1cf59ca211819011ccfa51446d9d1524d40
Катя <gitler.kaka@mai.ru>
2019-10-13 23:12:34 +0300

    UserService

---
+++ src/main/java/Lesson8/Application/UserService.java
@@ -0,0 +1,65 @@
+package Lesson8.Application;
+import java.util.*;
+
+
+public class UserService {
+    private Map<String,User> users;
+
+    public UserService() {
+        users = new HashMap<>();
+        User adminUser = new User("admin","admin", "Egor");
+        users.put("admin",adminUser);
+    }
+
+    public User getByLogin (String login){
+        if(users.containsKey(login)) {
+            System.out.println(users.get(login));
+        }
+        return users.get(login);
+    }
+
+    public boolean addUser(String login, User user){
+        if (users.get(login) == null) {
+            users.put(login, user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deleteByLogin (String login){
+        if(users.containsKey(login)){
+            users.remove(login);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public List<User> getAllUser() {
+        System.out.println(users.values());
+        return new ArrayList<>(users.values());
+    }
+
+    public boolean editUser (User user){
+        for (Map.Entry<String, User> pair: users.entrySet()) {
+            if(pair.getKey().equals(user.getLogin())) {
+                users.remove(user.getLogin());
+                users.put(user.getLogin(),user);
+                return true;
+            }
+        }
+        System.out.println("User not found");
+        return false;
+    }
+    public boolean auth(String login, String password){
+        if(users.containsKey(login)){
+            User user = users.get(login);
+            if(user.getPassword().equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
