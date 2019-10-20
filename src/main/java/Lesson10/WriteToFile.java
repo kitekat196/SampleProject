f7c0eafc7518d88cde163fbc77b1919e24dd07cd
Данила Морозов <89854821428@yandex.ru>
2019-10-20 12:00:09 +0300

    Edit User

--- src/main/java/Lesson10/WriteToFile.java
+++ src/main/java/Lesson10/WriteToFile.java
@@ -1,19 +1,23 @@
 package Lesson10;
+import com.github.javafaker.Faker;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Random;
 
 public class WriteToFile {
 	public static void main(String[] args) {
-		int a = 1;
-		int d = 1;
-		int c = 0;
+		Faker faker = new Faker(new Locale("ru"));
+		Random rand = new Random();
 		try (FileWriter writer = new FileWriter("src/main/java/resources/file.txt",false )){
 
-			for(int i = 0; i < 50; i++){
-				a = d + c;
-				d = c;
-				c = a;
-				writer.write(Integer.toString(a));
+			for(int i = 0; i < 10; i++){
+				String login = faker.name().firstName();
+				String pass = faker.name().lastName();
+				int age = rand.nextInt(30);
+				writer.write(login + " ");
+				writer.write(pass + " ");
+				writer.write(Integer.toString(age));
 				writer.append('\n');
 			}
 			writer.flush();
