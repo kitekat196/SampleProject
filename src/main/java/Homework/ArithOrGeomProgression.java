63776f4eb313d3e3d514f792a74ea53910dcb62b
Катя <gitler.kaka@mai.ru>
2019-10-24 21:32:27 +0300

    EditProgression

--- src/main/java/Homework/ArithOrGeomProgression.java
+++ src/main/java/Homework/ArithOrGeomProgression.java
@@ -1,13 +1,12 @@
 package Homework;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class ArithOrGeomProgression {
+
     public static boolean Progression(String path){
 
         List<Integer> numb = new ArrayList<>();
@@ -19,18 +18,21 @@ public class ArithOrGeomProgression {
             }
             int arifm;
             int geom;
-
-
             if (numb.size() > 1) {
                 arifm = numb.get(1) - numb.get(0);
-                geom = numb.get(1) / numb.get(0);
+                if(numb.get(0) > 0){
+                    geom = numb.get(1) / numb.get(0);
+                } else {
+                    geom = 0;
+                }
+
                 for (int i = 0; i < numb.size()-1; i++) {
                     if(arifm != (numb.get(i+1) - numb.get(i)) && geom != (numb.get(i+1) / numb.get(i))){
                         return false;
                     }
                 }
             } else {
-                System.out.println("Проверьте содержимое файла\nЦисел в файле должно быть больше одного и разделены пробелами");
+                System.out.println("Проверьте содержимое файла\\nЧисел в файле должно быть больше одного и разделены пробелами");
                 return false;
             }
 
